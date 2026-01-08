f34d5b475bcb0445923102220c88ece0af9b1ddb
Youssef Khaled <youssefkhaled001@outlook.com>
2026-01-08 18:57:49 +0200

    refactor: update Schedule import in console routes
    - Replace deprecated Illuminate\Console\Scheduling\Schedule
    - Use Illuminate\Support\Facades\Schedule instead
    - Laravel 12 compatibility update

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
