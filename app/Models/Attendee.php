2e115c56d516e7cd7b1a3a3d68b7c6b30b200082
Youssef Khaled <youssefkhaled001@outlook.com>
2026-01-10 23:10:25 +0200

    feat: add AttendeeFactory with realistic fake data

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Attendee extends Model
 {
+    use HasFactory;
     /**
      * The attributes that are mass assignable.
      *
